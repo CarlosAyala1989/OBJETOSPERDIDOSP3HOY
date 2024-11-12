6b5191ff2a00a25665a44cf2e5f8275685ba4cfa
RenzoA <renzoantayhua7@gmail.com>
2024-11-12 16:13:17 -0500

    Pinga

---
+++ src/java/Config/Conexion.java
@@ -0,0 +1,22 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Config;
+import java.sql.*;
+
+public class Conexion {
+        Connection con=null;
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://mysql9001.site4now.net:3306/db_aaf2ce_perdido", "aaf2ce_perdido", "perdido13");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("error conexion "+e.getMessage()); 
+        }
+    }
+    public Connection getConnection(){
+        return con;
+    }
+}
